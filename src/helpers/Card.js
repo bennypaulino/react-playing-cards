e58fe63029a917c12a9670a3307b87481c4c0603
bennypaulino <santiagopro71@gmail.com>
2017-05-27 12:22:14 -0400

    Add Lodash & Create helpers/Card.js

---
+++ src/helpers/Card.js
@@ -0,0 +1,11 @@
+export default class Card {
+
+  constructor(rank, suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  outputCard() {
+    console.log(`${this.rank} of ${this.suit}`);
+  }
+}
